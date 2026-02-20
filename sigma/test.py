771966657c14450246bb85dff3f52311f772ce2f
suryansh792 <rajawat.suryansh@gmail.com>
2026-02-20 17:05:17 -0500

    Updated to include name change of model

--- sigma/test.py
+++ sigma/test.py
@@ -9,12 +9,13 @@ import jax
 # Testing wavefunction.py
 N = 5
 x = np.random.normal(size=(N,2))
-model = psi(hidden_sizes=(64,64))
+model = MLP(hidden_sizes=(64,64))
 params = model.init(jax.random.PRNGKey(0), x)
 y = model.apply(params,x)
 
 print(f'Inputs: {x} \n Outputs: {y}')
 print(f'Inputs shape : {x.shape} \n Outputs shape {y.shape}')
+print(f'Hidden layer widths are {model.hidden_sizes}')
 
 cart_x = spherical_to_cartesian(x)
 print(f'Cartesian x, {cart_x} with shape {cart_x.shape}')
