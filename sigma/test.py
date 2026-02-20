f9d2da80980990b9cf480d5d7ba4706cb7d47fa3
Hersh Kumar <hershkumar13@gmail.com>
2026-02-20 17:11:40 -0500

    add sampling test

--- sigma/test.py
+++ sigma/test.py
@@ -13,9 +13,36 @@ model = MLP(hidden_sizes=(64,64))
 params = model.init(jax.random.PRNGKey(0), x)
 y = model.apply(params,x)
 
+def psi(params, config):
+    return jnp.exp(-model.apply(params, config))
+
+
+
 print(f'Inputs: {x} \n Outputs: {y}')
 print(f'Inputs shape : {x.shape} \n Outputs shape {y.shape}')
 print(f'Hidden layer widths are {model.hidden_sizes}')
 
 cart_x = spherical_to_cartesian(x)
 print(f'Cartesian x, {cart_x} with shape {cart_x.shape}')
+
+
+
+
+
+#=================
+# testing the sampler
+print("Testing sampler...")
+sampler = Sampler(psi, (N, 2))
+
+nchains = 16
+pos_initials = [jnp.zeros((N, 2)) for _ in range(nchains)]
+seeds = jnp.arange(nchains)
+var = 5
+
+samples, acc_rate = sampler.run_many_chains(params, 1000, 100, 2, var, pos_initials, seeds)
+
+print(f"Acceptance rate: {acc_rate}")
+print(samples.shape)
+#=================
+
+
